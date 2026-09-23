67cb40ee58e66e1465ade2e8aeb83a1d69ee4edb
gjparts <gjparts@gmail.com>
2026-09-23 10:14:49 -0600

    commit 2309202632665

---
+++ 03_Animacion/03_Animacion/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _03_Animacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //imprimir elementos en pantalla e irlos moviendo
+            for(int i = 1; i <= 100; i++)
+            {
+                Console.SetCursorPosition(i, 5);
+                Console.Write("><>");
+                Console.SetCursorPosition(i + 4, 10);
+                Console.Write("><>");
+                Console.SetCursorPosition(i + 8, 15);
+                Console.Write("><>");
+
+                Thread.Sleep(50); //pausa de 50ms
+                Console.Clear(); //Limpiar la pantalla
+            }
+        }
+    }
+}
